2bdc8b1cb2210ab35d7fde4d10d3add8e408142f
Sam Beam <sbeam@onsetcorps.net>
2010-09-30 22:03:24 -0400

    improve variable names and use each_with_index here..

--- lib/spree/dropdown_variants_by_option/variant.rb
+++ lib/spree/dropdown_variants_by_option/variant.rb
@@ -5,19 +5,17 @@ module Spree::DropdownVariantsByOption::Variant
       # Try and find the variant from the supplied pair option_type <--> option value and product
       def self.find_by_option_values(product_id, opt_values)
 
-        join = []
-        cond = {'variants.product_id' => product_id}
-        i = 0
+        join_clause = []
+        condition_clause = {'variants.product_id' => product_id}
 
-        opt_values.each_pair do |opt_type, opt_value|
-          join << "JOIN option_values_variants ovv#{i} ON ovv#{i}.variant_id = variants.id"
-          cond["ovv#{i}.option_value_id"] = opt_value
-          i += 1
+        opt_values.each_with_index do |opt_value, i|
+          join_clause << "JOIN option_values_variants ovv#{i} ON ovv#{i}.variant_id = variants.id"
+          condition_clause["ovv#{i}.option_value_id"] = opt_value
         end
 
         # sample SQL: SELECT "variants".* FROM "variants" JOIN option_values_variants ovv0 ON ovv0.variant_id = variants.id JOIN option_values_variants ovv1 ON ovv1.variant_id = variants.id WHERE ("variants"."product_id" = '569012001' AND "ovv0"."option_value_id" = '979459986' AND "ovv1"."option_value_id" = '369541888')
-        Variant.all(:joins => join.join(' '),
-                    :conditions => cond)
+        Variant.all(:joins => join_clause.join(' '),
+                    :conditions => condition_clause)
       end
     end
   end
