12dd9416bc2e436fa0b3c699f8307b49627f967a
Sam Beam <sbeam@onsetcorps.net>
2010-11-05 12:58:59 -0400

    refactor javascript to work better with my external modal dialog that happens on add to cart

--- public/javascripts/dropdown_variants_by_option.js
+++ public/javascripts/dropdown_variants_by_option.js
@@ -2,52 +2,42 @@ jQuery.ajaxSetup({
   'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
 })
 
-jQuery.fn.extend( {
-  submitWithAjax: function() {
-    this.submit(function(e) {
-      e.preventDefault();
-      $.post(this.action, $(this).serialize(), null, "script");
-      return false;
-      })
-      return this;
-    },
 
-    onChangeWithAjax: function() {
-      this.change(function(e) {
-        e.preventDefault();
-        $.post('/products/option_value_changed', $("#cart_form").serialize(), null, "script");
-        return false;
-      })
-      return this;
-    }
-});
+var spree_ddvbo_app = {
 
-
-var app =
-{
-  setupAjaxCallbacks: function () {
-    $('body').ajaxStart(function (){
-      $("#busy_indicator").show();
-    });
-    $('body').ajaxStop(function () {
-      $("#busy_indicator").hide();
-    });
+  addToCart: function(e) {
+          e.preventDefault();
+          spree_ddvbo_app.getBusy(null); // could really use $.proxy here but spree doesn't have 1.4
+          $.post(this.action, $(this).serialize(), spree_ddvbo_app.getNotBusy, "script");
+          return false;
   },
 
-  addToCart: function() {
-    $("#cart_form").submitWithAjax();
+  optionValueChanged: function(e) {
+          e.preventDefault();
+          spree_ddvbo_app.getBusy( function() {
+            spree_ddvbo_app.fetchValuesForSecondaryOptions( spree_ddvbo_app.getNotBusy );
+            return false;
+          });
   },
 
-  optionValueChanged: function() {
-    $("select#option_values_select_primary").onChangeWithAjax();
-    $("input#option_values_radio_primary").onChangeWithAjax();
+  fetchValuesForSecondaryOptions: function( fn_after ) {
+         $.post('/products/option_value_changed', $("#cart_form").serialize(), fn_after, "script");
+  },
+
+  getBusy : function( fn ) {
+          $("#busy_indicator").fadeIn('fast', fn);
+  },
+
+  getNotBusy : function() {
+          $("#busy_indicator").fadeOut('fast');
   }
-}
 
-jQuery( function () {
-  app.setupAjaxCallbacks();
+};
 
-  app.addToCart();
+jQuery(document).ready( function () {
 
-  app.optionValueChanged();
+  $("#cart_form").bind('submit', spree_ddvbo_app.addToCart);
+
+  $("#option_values_select_primary, #option_values_radio_primary").bind('change', spree_ddvbo_app.optionValueChanged);
+
 });
